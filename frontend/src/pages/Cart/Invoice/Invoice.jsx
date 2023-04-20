2365a606443ab4dd6f72aa7958cccb7eaacba947
harish kumar <harishkumarharishkumar108@gmail.com>
2023-04-20 13:52:38 +0530

    payment gateway added

--- frontend/src/pages/Cart/Invoice/Invoice.jsx
+++ frontend/src/pages/Cart/Invoice/Invoice.jsx
@@ -1,14 +1,52 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import { useSelector } from 'react-redux'
+import { toast } from 'react-toastify'
 
 import classes from './Invoice.module.css'
 const Invoice = () => {
+  const currentUser = useSelector((state) => state.currentUser.user)
   const cartItems = useSelector((state) => state.cart.cartItems)
 
   const totalAmount = cartItems.reduce(
     (acc, item) => acc + item.quantity * item.price,
     0,
   )
+
+  const paymentHandler = () => {
+    if (cartItems.length === 0) {
+      toast.error('Add some item to checkout')
+      return
+    }
+
+    var options = {
+      key: 'rzp_test_qzkbkoKqCTLxl7',
+      amount: totalAmount,
+      currency: 'INR',
+      name: 'Elecxo',
+      description: 'Total Produucts',
+      order_id: 'order_IluGWxBm9U8zJ8',
+      prefill: {
+        name: currentUser.name,
+        email: currentUser.email,
+        contact: currentUser.phoneNumber,
+      },
+      notes: {
+        address: 'Elexo Corp',
+      },
+      theme: {
+        color: '#3399cc',
+      },
+      handler: (response) => {
+        console.log(response)
+      },
+    }
+
+    const razorpay = new Razorpay(options)
+    razorpay.open()
+    razorpay.on('payment.failed', (response) => {
+      console.log(response)
+    })
+  }
 
   if (cartItems.length === 0) {
     return <h2 className="centered">No item in cart.</h2>
@@ -41,7 +79,9 @@ const Invoice = () => {
           </tr>
         </tbody>
       </table>
-      <button className={classes['pay-btn']}>Pay &#8377;{totalAmount}</button>
+      <button className={classes['pay-btn']} onClick={paymentHandler}>
+        Pay &#8377;{totalAmount}
+      </button>
     </div>
   )
 }
